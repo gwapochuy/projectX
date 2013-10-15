01b2f2e557d443cf6108add7f897571909e5cfba
Gwapo <gwapochuynuubsinc@gmail.com>
2013-10-15 15:20:09 +0000

    Update Binilaran.sh

--- Binilaran.sh
+++ Binilaran.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-tar -cvpzf wla.tar ambot
-a=$(find /home/ubuntu ! -path "/home/ubuntu/.*" -mmin -60 -user ubuntu)
+
+tar -cvpzf wla.tar emptyfile
+a=$(find /home ! -name '.*' -mmin -60 -user ubuntu)
 tar -cvpzf wla.tar $a
